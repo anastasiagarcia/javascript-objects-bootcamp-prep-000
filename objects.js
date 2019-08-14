e394d7a2f11932eab551ce9e2ac059487c6af338
Anastasia Pankina <anastaciap@ymail.com>
2019-08-14 16:24:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -32,3 +32,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist;
 }
 
+function removeFromPlaylist(playlist,artistName){
+  delete playlist[artistName];
+  return playlist;
+}
