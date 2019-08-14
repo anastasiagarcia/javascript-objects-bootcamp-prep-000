0f964158353425f6aca6de533da750c2822cd8f3
Anastasia Pankina <anastaciap@ymail.com>
2019-08-14 15:48:49 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,23 +1,23 @@
-//practicing 
-//adding to an Object
+// //practicing 
+// //adding to an Object
 
-var meals ={
-  breakfast: "oatmeal",
-  lunch:"burrito",
-  dinner:"steak"
-}
-//if we know the name of the key and its value. use dot syntax to add the new pair
+// var meals ={
+//   breakfast: "oatmeal",
+//   lunch:"burrito",
+//   dinner:"steak"
+// }
+// //if we know the name of the key and its value. use dot syntax to add the new pair
 
-meals.snack="yogurt";
+// meals.snack="yogurt";
 
-meals.snack; //yogurt
-meals["snack"]; //yogurt
+// meals.snack; //yogurt
+// meals["snack"]; //yogurt
 
-//adding key-value pairs using bracker notation
+// //adding key-value pairs using bracker notation
 
-meals["second breakfast"] = "bagel";
+// meals["second breakfast"] = "bagel";
 
-// to chaange the value
-meals.breakfast="cereal";
+// // to chaange the value
+// meals.breakfast="cereal";
 
-//all of the changes above are destructive
+// //all of the changes above are destructive
